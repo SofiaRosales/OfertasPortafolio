487a984cfba2a15e1ca4079311f7c0032bdeefed
Sergio Cerda <sergio.cerda21@outlook.com>
2018-05-15 18:04:51 -0400

    éncargado de tiendav1

---
+++ SolucionOfertas/Controllers/Enc_TiendaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SolucionOfertas.Controllers
+{
+    public class Enc_TiendaController : Controller
+    {
+        // GET: Enc_Tienda
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // Rutas
+        public ActionResult Adm_producto()
+        {
+            return View();
+        }
+        public ActionResult ADD_producto()
+        {
+            return View();
+        }
+    }
+}
