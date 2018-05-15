e024e1e7fccc4370ad48e7f5ff5c4ee69d7ca32d
Sergio Cerda <sergio.cerda21@outlook.com>
2018-05-15 16:57:21 -0400

    Datos personales registro(Prueba Boostrap)

--- SolucionOfertas/Controllers/AccountController.cs
+++ SolucionOfertas/Controllers/AccountController.cs
@@ -151,6 +151,7 @@ namespace SolucionOfertas.Controllers
         {
             if (ModelState.IsValid)
             {
+                //CAMBIAR
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
@@ -163,7 +164,7 @@ namespace SolucionOfertas.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirmar cuenta", "Para confirmar la cuenta, haga clic <a href=\"" + callbackUrl + "\">aquí</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Registro");
                 }
                 AddErrors(result);
             }
