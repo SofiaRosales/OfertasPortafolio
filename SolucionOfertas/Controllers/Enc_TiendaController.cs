aed22d1dc6cceba308330ab3af849fa61f051acb
Sergio Cerda <sergio.cerda21@outlook.com>
2018-05-16 17:18:20 -0400

    Llenar DropDownList desde BD

--- SolucionOfertas/Controllers/Enc_TiendaController.cs
+++ SolucionOfertas/Controllers/Enc_TiendaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SolucionOfertas.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -22,7 +23,40 @@ namespace SolucionOfertas.Controllers
         }
         public ActionResult ADD_producto()
         {
+            EntitiesOfertas entityOferta = new EntitiesOfertas();
+            var getCategoriasList = entityOferta.CATEGORIA.ToList();
+            SelectList list = new SelectList(getCategoriasList, "ID","NOMBRE");
+            ViewBag.categoriaListName = list;
+            
+            var getSubCategoriasList = entityOferta.SUBCATE.ToList();
+            SelectList list2 = new SelectList(getSubCategoriasList, "ID", "NOMBRE");
+            ViewBag.SubCategoriaListName = list2;
+
             return View();
         }
+        public ActionResult ADD_oferta()
+        {
+            return View();
+        }
+
+        // Cargar Select de ADD_producto
+        //public JsonResult getcategorias()
+        //{
+        //    var query = from c in db.Categorie
+        //                select new { c.CategoryID, c.CategoryName };
+
+        //    return Json(query, JsonRequestBehavior.AllowGet);
+
+        //}
+        //public JsonResult getsuppliers()
+        //{
+        //    var query = from s in db.Suppliers
+        //                select new { s.SupplierID, s.CompanyName };
+
+        //    return Json(query, JsonRequestBehavior.AllowGet);
+
+        //}
+
+
     }
 }
