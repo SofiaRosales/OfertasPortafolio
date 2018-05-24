cf6c6d7068c76b5ef2d9834c8d8195c7dd456e80
Francisco <Francisco@TRON>
2018-05-24 19:08:44 -0400

    :(

--- SolucionOfertas/Controllers/ConsumidorController.cs
+++ SolucionOfertas/Controllers/ConsumidorController.cs
@@ -19,7 +19,25 @@ namespace LoginOfertas3.Controllers
         {
             return View();
         }
+
+        public ActionResult add_descuento(decimal porcentaje)
+        {
+            entityOferta.ADD_DESCUENTO_PROCEDURE(porcentaje);
+            return RedirectToAction("Cupon");
+        }
+
+        public ActionResult add_cupon(decimal descuento_id, decimal perfil_persona_id, decimal descuento)
+        {
+            entityOferta.ADD_CUPON_PROCEDURE(descuento_id, perfil_persona_id, descuento);
+            return RedirectToAction("Puntos");
+        }
+
         public ActionResult Puntos()
+        {
+            //como mostrar los puntos de la sesion activa?
+            return View();
+        }
+        public ActionResult Cupon()
         {
             return View();
         }
